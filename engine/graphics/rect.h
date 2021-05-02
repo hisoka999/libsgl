c62795ab10d6a1c92479e0d1aaef2f46bbb77508
hisoka999 <hisoka999@gmail.com>
2021-05-02 17:55:07 +0200

    added comperators to rect type

--- engine/graphics/rect.h
+++ engine/graphics/rect.h
@@ -3,71 +3,78 @@
 
 #include <SDL2/SDL.h>
 #include <engine/utils/vector2.h>
-namespace graphics{
+namespace graphics
+{
 
-struct Rect{
-    float x;
-    float y;
-    float width;
-    float height;
-
-    Rect() {
-        x = 0;
-        y = 0;
-        width = 0;
-        height = 0;
-    }
-    Rect(float pX, float pY, float pWidth, float pHeight) {
-        x = pX;
-        y = pY;
-        width = pWidth;
-        height = pHeight;
-    }
-    bool intersectsNoLine(const Rect& target)
+    struct Rect
     {
-        if (
-            (x + width  > target.x) &&
-            (y + height > target.y) &&
-            (x < target.x + target.width) &&
-            (y < target.y + target.height)
-            )
+        float x;
+        float y;
+        float width;
+        float height;
+
+        Rect()
+        {
+            x = 0;
+            y = 0;
+            width = 0;
+            height = 0;
+        }
+        Rect(float pX, float pY, float pWidth, float pHeight)
+        {
+            x = pX;
+            y = pY;
+            width = pWidth;
+            height = pHeight;
+        }
+        bool intersectsNoLine(const Rect &target) const
+        {
+            if (
+                (x + width > target.x) &&
+                (y + height > target.y) &&
+                (x < target.x + target.width) &&
+                (y < target.y + target.height))
                 return true;
-        else
-            return false;
-    }
-    bool intersects(const Rect& target)
-    {
-        if (
-            (x + width  >= target.x) &&
-            (y + height >= target.y) &&
-            (x <= target.x + target.width) &&
-            (y <= target.y + target.height)
-            )
+            else
+                return false;
+        }
+        bool intersects(const Rect &target) const
+        {
+            if (
+                (x + width >= target.x) &&
+                (y + height >= target.y) &&
+                (x <= target.x + target.width) &&
+                (y <= target.y + target.height))
                 return true;
-        else
+            else
+                return false;
+        }
+        bool intersects(const utils::Vector2 &v)
+        {
+            if (x <= v.getX() && y <= v.getY() && x + width >= v.getX() && y + height >= v.getY())
+                return true;
             return false;
-    }
-    bool intersects(const utils::Vector2& v)
-    {
-        if(x <= v.getX()
-           && y <= v.getY()
-           && x+width >= v.getX()
-           && y+height >= v.getY()
-           )
-            return true;
-        return false;
-    }
+        }
 
-    SDL_Rect sdlRect()
-    {
-        SDL_Rect r;
-        r.h = height;
-        r.w = width;
-        r.x = x;
-        r.y = y;
-        return r;
-    }
-};
+        SDL_Rect sdlRect()
+        {
+            SDL_Rect r;
+            r.h = height;
+            r.w = width;
+            r.x = x;
+            r.y = y;
+            return r;
+        }
+
+        bool operator==(const Rect &other) const
+        {
+            return other.x == x && other.y == y && other.width == width && other.height == height;
+        }
+        bool operator!=(const Rect &other) const
+        {
+            return other.x != x || other.y != y || other.width != width || other.height != height;
+        }
+    };
 
 }
 
