b51f21b20634176f914531211e2528ce3ee3424b
hisoka999 <hisoka999@gmail.com>
2021-07-10 21:07:44 +0200

    added basic time api (unfinished)

---
+++ engine/utils/time/date.cpp
@@ -0,0 +1,53 @@
+#include "date.h"
+#include <engine/utils/string.h>
+namespace utils
+{
+    namespace time
+    {
+        const uint16_t lastDayOfMonth(const uint16_t year, const uint16_t month)
+        {
+            if (month == 1)
+            {
+                if (isLeapYear(year))
+                    return 29;
+                return 28;
+            }
+            else if (month % 2)
+            {
+                return 30;
+            }
+            else
+            {
+                return 31;
+            }
+        }
+        const bool isLeapYear(const uint16_t year)
+        {
+            return false; //TODO implement it the right way
+        }
+        Date::Date(uint16_t year, uint16_t month, uint16_t day)
+            : year(year), month(month), day(day)
+        {
+        }
+
+        void Date::addDay(uint16_t value)
+        {
+            day += value;
+            while (lastDayOfMonth(year, month) > day)
+            {
+                day -= lastDayOfMonth(year, month);
+                month++;
+                if (month > 11)
+                {
+                    month = 0;
+                    year++;
+                }
+            }
+        }
+
+        std::string Date::format()
+        {
+            return utils::string_format("%2i.%2i.%i", day, month + 1, year);
+        }
+    }
+}
---
+++ engine/utils/time/date.h
@@ -0,0 +1,33 @@
+#ifndef __DATE_H__
+#define __DATE_H__
+
+#include <cstdint>
+#include <string>
+
+namespace utils
+{
+    namespace time
+    {
+        const uint16_t lastDayOfMonth(const uint16_t year, const uint16_t month);
+        const bool isLeapYear(const uint16_t year);
+
+        class Date
+        {
+        private:
+            uint16_t year;
+            uint16_t month;
+            uint16_t day;
+
+        public:
+            Date(uint16_t year, uint16_t month, uint16_t day);
+            ~Date();
+            void addDay(uint16_t value);
+
+            std::string format();
+        };
+
+    } // namespace time
+
+} // namespace utils
+
+#endif // __DATE_H__
