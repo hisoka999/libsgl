9cd5d4a09ba35d34e9d8b81a91c5ddff1d6d9a59
hisoka999 <hisoka999@gmail.com>
2022-05-02 16:59:58 +0200

    fix table component errors

--- engine/ui/Table.h
+++ engine/ui/Table.h
@@ -24,8 +24,9 @@ namespace UI
 
         void setHeaderNames(const std::vector<std::string> &headerNames);
 
-        void setElementFunction(size_t col, std::function<std::string(std::shared_ptr<T>)> displayFunction)
+        void setElementFunction(size_t col, std::function<std::string(std::shared_ptr<T> &)> displayFunction)
         {
+            m_cellRenderer.resize(m_headerNames.size());
             m_cellRenderer[col] = displayFunction;
         }
 
@@ -33,12 +34,88 @@ namespace UI
         /* data */
         std::vector<std::shared_ptr<T>> m_data;
         std::vector<std::string> m_headerNames;
-        std::vector<std::function<std::string(std::shared_ptr<T>)>> m_cellRenderer;
+        std::vector<std::function<std::string(std::shared_ptr<T> &)>> m_cellRenderer;
 
         SDL_Color m_borderColor;
         SDL_Color m_headerColor;
+        SDL_Color m_textColor;
     };
 
 } // namespace UI
+#include "engine/graphics/TextureManager.h"
+
+namespace UI
+{
+    template <typename T>
+    Table<T>::Table(UI::Object *parent)
+        : UI::Object(parent)
+    {
+        setObjectName("table");
+        if (parent == nullptr || getTheme() == nullptr)
+        {
+            setTheme(graphics::TextureManager::Instance().getDefaultTheme());
+        }
+
+        m_borderColor = getTheme()->getStyleColor(this, StyleType::BorderColor);
+        m_headerColor = getTheme()->getStyleColor(this, StyleType::TitleColor);
+        m_textColor = getTheme()->getStyleColor(this, StyleType::Color);
+    }
+
+    template <typename T>
+    Table<T>::~Table()
+    {
+    }
+
+    template <typename T>
+    void Table<T>::render(core::Renderer *renderer)
+    {
+
+        // render header
+        renderer->setDrawColor(m_borderColor);
+        renderer->drawRect(displayRect());
+        //
+        int x = displayRect().x + 10;
+
+        int yHeader = displayRect().y;
+        const float rowHeight = 25.f;
+        for (std::string &header : m_headerNames)
+        {
+
+            getFont()->render(renderer, header, m_headerColor, x, yHeader + 5);
+            x += 100; // for now fixed width
+        }
+        yHeader += rowHeight;
+        for (size_t row = 0; row < m_data.size(); ++row)
+        {
+            auto &value = m_data[row];
+            int y = yHeader + (float(row) * rowHeight);
+
+            for (size_t col = 0; col < m_cellRenderer.size(); ++col)
+            {
+                int x = displayRect().x + (col * 100) + 10;
+
+                std::string cellValue = m_cellRenderer[col](value);
+                getFont()->render(renderer, cellValue, m_textColor, x, y + 5);
+            }
+            utils::Vector2 lineStart(displayRect().x, yHeader + (float(row) * rowHeight));
+            utils::Vector2 lineEnd(displayRect().x + displayRect().width, yHeader + (float(row) * rowHeight));
+            renderer->drawLine(lineStart, lineEnd);
+        }
+    }
+
+    template <typename T>
+    void Table<T>::handleEvents(core::Input *input)
+    {
+
+        UI::Object::handleEvents(input);
+    }
+
+    template <typename T>
+    void Table<T>::setHeaderNames(const std::vector<std::string> &headerNames)
+    {
+        m_headerNames = headerNames;
+    }
+
+}
 
 #endif
