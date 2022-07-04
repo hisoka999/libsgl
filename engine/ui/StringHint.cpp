279cae5402133375d185344e998562f401bd454f
hisoka999 <hisoka999@gmail.com>
2022-07-04 23:04:41 +0200

    updating string hint

--- engine/ui/StringHint.cpp
+++ engine/ui/StringHint.cpp
@@ -6,7 +6,7 @@ namespace UI
 {
     StringHint::StringHint() : Hint()
     {
-        font = graphics::TextureManager::Instance().loadFont("fonts/arial.ttf", 12);
+        font = graphics::TextureManager::Instance().loadFont("fonts/arial.ttf", 14);
     }
 
     StringHint::~StringHint()
@@ -15,22 +15,35 @@ namespace UI
 
     void StringHint::render(core::Renderer *renderer)
     {
-        int w, h;
-        font->size(getHintText(), &w, &h);
+        int w, h = 0;
 
         int lineCount = 1;
+        int lastLineBreak = 0;
+        int pos = 0;
+        int maxWidth = 0;
         for (char chr : getHintText())
         {
-            if (chr == '\n')
-                lineCount++;
-        }
 
-        setWidth(w);
+            if (chr == '\n')
+            {
+                lineCount++;
+                std::string line = getHintText().substr(lastLineBreak, pos - lastLineBreak);
+                font->size(line, &w, &h);
+                maxWidth = std::max(w, maxWidth);
+                lastLineBreak = pos;
+            }
+            pos++;
+        }
+        if (lastLineBreak == 0)
+            font->size(getHintText(), &w, &h);
+
+        maxWidth = std::max(w, maxWidth);
+        setWidth(maxWidth);
         setHeight(h * lineCount);
 
         renderBackground(renderer);
 
-        font->renderWrapped(renderer, getHintText(), utils::color::BLUE, getPosition().getX(), getPosition().getY(), 150);
+        font->renderWrapped(renderer, getHintText(), utils::color::WHITE, getPosition().getX(), getPosition().getY(), 200);
     }
 
 } // namespace UI
